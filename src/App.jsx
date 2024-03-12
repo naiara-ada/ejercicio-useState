2ac1bca672261f989c97b56b7f6263a1bae91e24
naiara-ada <naiara.ada@gmail.com>
2024-03-12 11:41:00 +0100

    working

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,33 @@
 import './App.css'
+import {useState} from 'react';
+
 
 function App() {
  //Aquí tu código
+const [name, setName] = useState('Sofia');
+const [newName, setNewName] = useState('');
+
+
+const setTeacher = (newTeacherName) => {
+    setName(newTeacherName);
+  };
+
+
+return(
+    <div>
+        <h2>Teacher Name: {name}</h2>
+        <ul>
+            <li onClick={() => setTeacher("Data")}>Data</li>
+            <li onClick={() => setTeacher("Reyes")}>Reyes</li>
+            <li onClick={() => setTeacher("Yolanda")}>Yolanda</li>
+            <li onClick={() => setTeacher("Sofia")}>Sofia</li>        
+        </ul>
+    </div>
+)
+
 }
 
 export default App
+
+
+
